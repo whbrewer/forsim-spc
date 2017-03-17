d24bf22fbcea1fe226f90442027785355818d36a
Wes Brewer <wes@fluidphysics.com>
2017-03-17 22:06:49 +0830

    get working for multiple chromos, genes, phenos, and pops

--- forsim.js
+++ forsim.js
@@ -11,7 +11,7 @@ function addPopulation() {
     count += 1
 
     var newdiv = document.createElement("div")
-    newdiv.id = 'pop'+count
+    newdiv.id = 'pop' + count
     newdiv.className = 'form-group'
     newdiv.style = 'margin:5px'
 
@@ -44,33 +44,37 @@ function addChromosome() {
 }
 
 function addPhenotype() {
+    pheno_count += 1
+
     var newdiv = document.createElement("div")
-    newdiv.id = 'pop' + count
+    newdiv.id = 'pheno' + pheno_count
     newdiv.className = 'form-group'
     newdiv.style = 'margin:5px'
 
-    pheno_count += 1
+    console.log(pheno_count)
 
     newdiv.appendChild(createInput('pheno_name', 'text', 'pheno' + pheno_count))
+    newdiv.appendChild(createInput('pheno_def', 'text', 'ABC1 + ABC2 * ifMale + 0.3 * ifFemale', 'width:600px; margin:5px'))
 
-    if (pheno_count % 2 == 0) {
-        newdiv.appendChild(createInput('pheno_def', 'text', 'ABC1 + ABC2 * ifMale + 0.3 * ifFemale', 'width:600px; margin:5px'))
-    } else {
-        // (3.0 * ABC1) + ((ABC2 / 2.0) / 2.0) # * environment + 2.0 * familyEnvironment
-        newdiv.appendChild(createInput('pheno_def', 'text', 'ABC1', 'width:600px; margin:5px'))
-    }
+    // if (pheno_count % 2 == 0) {
+    //     newdiv.appendChild(createInput('pheno_def', 'text', 'ABC1 + ABC2 * ifMale + 0.3 * ifFemale', 'width:600px; margin:5px'))
+    // } else {
+    //     // (3.0 * ABC1) + ((ABC2 / 2.0) / 2.0) # * environment + 2.0 * familyEnvironment
+    //     newdiv.appendChild(createInput('pheno_def', 'text', 'ABC1', 'width:600px; margin:5px'))
+    // }
 
     var mydiv = document.getElementById("phenotype")
     mydiv.appendChild(newdiv)
 }
 
 function addGene(mydiv) {
+    gene_count += 1
+
     var newdiv = document.createElement("div")
-    newdiv.id = 'pop' + count
+    newdiv.id = 'gene' + gene_count
     newdiv.className = 'form-group'
     newdiv.style = 'margin:5px'
 
-    gene_count += 1
     gene_location = (gene_count+2)*1E6
 
     newdiv.appendChild(createInput('gene_name', 'text', 'ABC' + gene_count))
@@ -86,6 +90,7 @@ function addGene(mydiv) {
 function createInput(name, type, value, style='width:150px; margin:5px') {
     input = document.createElement('input')
     input.type = type
+    input.id = name
     input.name = name
     input.placeholder = name
     input.value = value
@@ -112,8 +117,6 @@ function createLabel(text) {
 function updateScript() {
     script = document.getElementById("input_script")
     form = document.input_form
-    // lb = '&#10;' //'&#xA;'
-    // lb = "\n"
     lb = "&#xA;"
 
     buf = "global begin" + lb
@@ -141,47 +144,78 @@ function updateScript() {
     }
     buf += "end" + lb + lb
 
-    // chromosome
-    buf += "chromosome begin" + lb
-    buf += "length " + form.chrom_length.value + lb + lb
+    var chromosomes = $('#chromosome').children('div')
 
-        // gene
+    for( i=0; i< chromosomes.length; i++ )
+    {
+     var chromosome = chromosomes[i];
+     // console.log(chromosome)
+     // chromosome
+     buf += "chromosome begin" + lb
+     var selector = '#'+chromosome.id
+
+     // surely there is a better way to do this
+     var chrome_length = $(selector).children('input')[0].value
+     buf += "length " + chrome_length + lb + lb
+
+     var selector = '#'+chromosome.id
+     var genes = $(selector).children('div')
+     label = ["name ", "location ", "length ", "gamma ", "probabilityNoEffect ", "probabilityPositiveEffect "]
+
+     for (j = 0; j< genes.length; j++) {
+        gene = genes[j]
+        var gene_chars = $('#'+gene.id).children('input')
+
         buf += "gene begin" + lb
-        buf += "name " + form.gene_name.value + lb
-        buf += "location " + form.gene_location.value + lb
-        buf += "length " + form.gene_length.value + lb
-        buf += "gamma " + form.gene_gamma.value + lb
-        buf += "probabilityNoEffect " + form.gene_probability_no_effect.value + lb
-        buf += "probabilityPositiveEffect " + form.gene_probability_pos_effect.value + lb
+
+        for (k = 0; k < gene_chars.length; k++) {
+            char = gene_chars[k]
+            buf += label[k] + char.value + lb
+        }
         buf += "end" + lb + lb
+     }
+     buf += "end" + lb + lb
 
-    buf += "end" + lb + lb
+    }
 
-    // phenotype
-    buf += "phenotype begin" + lb
-    buf += "name " + form.pheno_name.value + lb
-    buf += "definition " + form.pheno_def.value + lb
-    buf += "end" + lb + lb
+    var phenotypes = $('#phenotype').children('div')
+    var pheno_label = ["name ", "definition "]
 
-    // population
-    buf += "population begin" + lb
-    buf += "name " + form.pop_name.value + lb
-    buf += "birth " + form.pop_birth.value + lb
-    buf += "carryingCapacity " + form.pop_carrying_capacity.value + lb
-    buf += "initialSize " + form.pop_init_size.value + lb
-    buf += "growthRate " + form.pop_growth_rate.value + lb
-    buf += "death " + form.pop_death.value + lb
+    for( i=0; i < phenotypes.length; i++ )
+    {
+        buf += "phenotype begin" + lb
+        var pheno = phenotypes[i];
+        console.log(pheno)
+        var pheno_inputs = $('#'+pheno.id).children('input')
+        for (k = 0; k < pheno_inputs.length; k++) {
+            char = pheno_inputs[k]
+            buf += pheno_label[k] + char.value + lb
+        }
+        buf += "end" + lb + lb
+    }
 
-    buf += "selection PhenotypeA relative -4.8 4.8" + lb
-    buf += "environmentNormal PhenotypeA 0.0 1.0" + lb
-    buf += "mateCutoff internal PhenotypeA -4.8 4.8" + lb
-    buf += "mateCutoff external PhenotypeA -4.8 4.8" + lb
-    // buf += "selection PhenotypeB relative -4.8 4.8" + lb
-    // buf += "environmentNormal PhenotypeB 0.0 1.0" + lb
-    // buf += "mateCutoff internal PhenotypeB -4.8 4.8" + lb
-    // buf += "mateCutoff external PhenotypeB -4.8 4.8" + lb
+    var populations = $('#population').children('div')
+    var pop_label = ["name ", "birth ", "carryingCapacity ", "initialSize ", "growthRate ", "death "]
 
-    buf += "end" + lb
+    for( i=0; i< populations.length; i++ )
+    {
+        buf += "population begin" + lb
+        var population = populations[i];
+        var pop_inputs = $('#'+population.id).children('input')
+        for (k = 0; k < pop_inputs.length; k++) {
+            char = pop_inputs[k]
+            buf += pop_label[k] + char.value + lb
+        }
+        buf += "selection PhenotypeA relative -4.8 4.8" + lb
+        buf += "environmentNormal PhenotypeA 0.0 1.0" + lb
+        buf += "mateCutoff internal PhenotypeA -4.8 4.8" + lb
+        buf += "mateCutoff external PhenotypeA -4.8 4.8" + lb
+        // buf += "selection PhenotypeB relative -4.8 4.8" + lb
+        // buf += "environmentNormal PhenotypeB 0.0 1.0" + lb
+        // buf += "mateCutoff internal PhenotypeB -4.8 4.8" + lb
+        // buf += "mateCutoff external PhenotypeB -4.8 4.8" + lb
+        buf += "end" + lb + lb
+    }
 
     script.innerHTML = buf
 
